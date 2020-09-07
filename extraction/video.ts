43f0e39c8f80f55df5f77cac7c59b9ca25b78181
Jonn Spencer <jonn@jonnjonnjonn.com>
2020-09-07 17:07:58 +0900

    Goes faster now!

--- extraction/video.ts
+++ extraction/video.ts
@@ -28,6 +28,8 @@ export interface Interval {
 }
 
 export class Video {
+  stream: fs.WriteStream;
+
   extractionProgress: BehaviorSubject<ExtractionStatus> = new BehaviorSubject({ uri: this.videoPath, phase: 'NOT_STARTED', percentage: 0 });
 
   constructor(private videoPath: string, private scratchPath: string, private ffpaths: FfpathsConfig) {
@@ -49,10 +51,10 @@ export class Video {
   }
 
   async extractDialog(): Promise<any> {
-    fs.mkdirSync(this.scratchPath, { recursive: true }) //todo: async
+    fs.mkdirSync(this.scratchPath, { recursive: true });
+    this.stream = fs.createWriteStream(`${path.basename(this.videoPath, path.extname(this.videoPath))}.mp3`);
     await this.extractSubtitles();
     const val = await this.getSubtitleIntervals();
-    // const result = await this.copyAudio();
     const combined = this.combineIntervals(val);
     await this.extractSegment(combined);
     console.error('marking as resolved');
@@ -74,104 +76,28 @@ export class Video {
 
   /** Synchronously extracts segments. */
   private async extractSegment(intervals: Interval[]) {
-    // Try it in one command?
-    // const command = ffmpeg(path.join(this.scratchPath, `full_audio.mp3`))//(this.videoPath)
-    //   .noVideo() //opt
-    //   // .outputOption(`-ss`, `${interval.start}`, `-to`, `${interval.end}`, "-q:a", "0", "-map", "a")
-    //   .audioBitrate('128k') //opt
-    //   .audioCodec('libmp3lame') //opt
-    // // .saveToFile(path.join(this.scratchPath, `shard-${i}.mp3`));
 
-    // for (let i = 0; i < intervals.length; i++) {
-    //   const interval = intervals[i];
-    //   command
-    //     .seekInput(interval.start)
-    //     .output(path.join(this.scratchPath, `shard-${i}a.mp3`))
-    //     .seek(interval.end);
-    // }
-
-    // command
-    //   .on('progress', progress => {
-    //     console.error('A progress is occurring', progress.percent)
-    //     this.extractionProgress.next({
-    //       uri: this.videoPath, phase: 'SPLITTING',
-    //       percentage: progress.percent, //((1 / intervals.length) * (+progress.percent / 100) + i) * 100 / intervals.length,
-    //     });
-    //   });
-
-    // await this.runCommand(command);
-
-    // this.extractionProgress.next({
-    //   uri: this.videoPath, phase: 'SPLITTING_DONE',
-    //   percentage: 100,
-    // });
-
-    let joinCommand = ffmpeg();
-
-    // THIS IS THE GOOD ONE HERE:
-    for (let i = 0; i < intervals.length; i++) {
+    for (let i = 0, max = intervals.length; i < max; i++) {//intervals.length; i++) {
       const interval = intervals[i];
-      const command = ffmpeg(this.videoPath)//(path.join(this.scratchPath, `full_audio.mp3`))
-        .noVideo() //opt
-        .outputOption(`-ss`, `${interval.start}`, `-to`, `${interval.end}`, "-q:a", "0", "-map", "a")
-        .audioBitrate('128k') //opt
-        .audioCodec('libmp3lame') //opt
-        .saveToFile(path.join(this.scratchPath, `shard-${i}.mp3`))
+      const command = ffmpeg(this.videoPath)
+        .noVideo()
+        .outputOption(`-ss`, `${interval.start}`, `-to`, `${interval.end}`)//, "-q:a", "0", "-map", "a")
+        .audioBitrate('128k')
+        .audioCodec('libmp3lame')
+        .format('mp3')
         .on('progress', progress => {
           this.extractionProgress.next({
-            uri: this.videoPath, phase: 'SPLITTING',
+            uri: this.videoPath, phase: 'JOINING',
             percentage: ((1 / intervals.length) * (+progress.percent / 100) + i) * 100 / intervals.length,
           });
         });
-      await this.toPromise(command, command => command.run());
-
-      joinCommand = joinCommand.mergeAdd(path.join(this.scratchPath, `shard-${i}.mp3`));
+      await this.toPromise(command, command => command.pipe(this.stream, i === max - 1 ? { end: true } : { end: false }));
     }
 
-    console.error('JOINING THEM TOGETHER');
-    //shell.ExecuteCommand(l, "ffmpeg", "-y", "-f", "concat", "-safe", "0", "-i", tempDir+"output.txt", "-c", "copy", tempDir+audioOutPath); err != nil {
-    await this.toPromise(
-      joinCommand.on('progress', progress => {
-        this.extractionProgress.next({
-          uri: this.videoPath, phase: 'JOINING',
-          percentage: progress.percent,
-        });
-      }),
-      command => command.mergeToFile(`${path.basename(this.videoPath, path.extname(this.videoPath))}.mp3`)
-    );
-    console.error('JOINED THEM TOGETHER');
     this.extractionProgress.next({
       uri: this.videoPath, phase: 'DONE',
       percentage: 100,
     });
-
-    // joinCommand = joinCommand
-    //   // .noVideo() //opt
-    //   // // .outputOption('-f', 'concat', "-q:a", "0", "-map", "a")
-    //   // .audioBitrate('128k') //opt
-    //   // .audioCodec('libmp3lame') //opt
-
-    //   .on('error', (err, stdout: string, stderr: string) => {
-    //     console.error('SOMETHING WENT WRONG', err, stdout, stderr);
-    //     // reject({ stdout: stdout, stderr: stderr });
-    //   })
-    //   .on('end', (stdout: string, stderr: string) => {
-    //     // resolve({ stdout: stdout, stderr: stderr });
-    //     console.error('JOINED THEM TOGETHER');
-    //     this.extractionProgress.next({
-    //       uri: this.videoPath, phase: 'DONE',
-    //       percentage: 100,
-    //     });
-    //   })
-    //   .on('progress', progress => {
-    //     this.extractionProgress.next({
-    //       uri: this.videoPath, phase: 'JOINING',
-    //       percentage: progress.percent,
-    //     });
-    //   })
-    //   .mergeToFile(`${path.basename(this.videoPath, path.extname(this.videoPath))}.mp3`);
-    // // await this.runCommand(joinCommand);
-
   }
 
   private async extractSubtitles() {
@@ -209,30 +135,12 @@ export class Video {
       combined.push(pending)
     }
 
-    let fixed = '';
-    combined.forEach(c => {
-      fixed += `\n ${c.start} - ${c.end}`;
-    });
-    fs.writeFile(path.join(this.scratchPath, 'subs_fixed.srt'), fixed, () => { });
+    // let fixed = '';
+    // combined.forEach(c => {
+    //   fixed += `\n ${c.start} - ${c.end}`;
+    // });
+    // fs.writeFile(path.join(this.scratchPath, 'subs_fixed.srt'), fixed, () => { });
     return combined;
-  }
-
-  private async copyAudio() {
-    return this.toPromise(
-      ffmpeg(this.videoPath)
-        .noVideo() //opt
-        .outputOption("-q:a", "0", "-map", "a")
-        .audioBitrate('128k') //opt
-        .audioCodec('libmp3lame') //opt
-        .saveToFile(path.join(this.scratchPath, `full_audio.mp3`))
-        .on('progress', progress => {
-          this.extractionProgress.next({
-            uri: this.videoPath, phase: 'EXTRACTING_AUDIO',
-            percentage: progress.percent,
-          });
-        }),
-      command => command.run()
-    );
   }
 
   private getSubtitleIntervals(): Promise<Interval[]> {
