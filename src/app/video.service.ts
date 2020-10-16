cbe552c639957ecca99eef9e9e7c7202b1fc4d35
Jonn Spencer <jonn@jonnjonnjonn.com>
2020-10-16 22:07:01 +0900

    1.5s threshold

--- src/app/video.service.ts
+++ src/app/video.service.ts
@@ -14,6 +14,8 @@ import {
 import { ExtractionStatus, VideoModel } from './shared/models/video-model';
 import { sortOnField } from './shared/sort';
 
+const GAP_THRESHOLD = moment.duration(1500);
+
 export interface VideoExtractionConfig {
   video: VideoModel;
   subtitleStream?: ffmpeg.FfprobeStream;
@@ -41,8 +43,9 @@ const extractSrtSubtitleIntervals = (subtitles: string): Interval[] => {
 
 const isGapOverThreshold = (start: string, end: string) => {
   return (
-    moment.duration(end).subtract(moment.duration(start)).milliseconds() > 150
-  ); // todo threshold
+    moment.duration(end).subtract(moment.duration(start)).milliseconds() >
+    GAP_THRESHOLD.milliseconds()
+  );
 };
 
 /** Merges overlapping intervals and sorts. */
